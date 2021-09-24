6a18993ddcdf9a3c56f11d8c6b948b27794cd8a8
Nils Petter Skålerud <np_skalerud@hotmail.com>
2021-09-24 17:09:13 +0200

    Fixed typo in inner assume macro.
    Signed-off-by: Nils Petter Skålerud <np_skalerud@hotmail.com>

--- any/include/DEngine/impl/InnerAssert.hpp
+++ any/include/DEngine/impl/InnerAssert.hpp
@@ -10,7 +10,7 @@
 
 // Define assume
 // Don't use directly
-#ifdef _MSC_BUILD
+#ifdef _MSC_VER
 #	define DENGINE_IMPL_INNER_ASSUME(condition) __assume(condition)
 #else
 #	define DENGINE_IMPL_INNER_ASSUME(condition) ((condition) ? static_cast<void>(0) : DENGINE_IMPL_INNER_UNREACHABLE())
